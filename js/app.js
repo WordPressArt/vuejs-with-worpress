1f0e75220f63a6b1d0fd2d01e5cacf59737fd8e4
Sanjaya Dulal <wwwdevwp@gmail.com>
2016-12-11 23:25:18 +0545

    remove space

--- js/app.js
+++ js/app.js
@@ -1,20 +1,14 @@
 var App = Vue.extend({});
-
-
 var postList = Vue.extend({
-	template:"<h1>hello world</h1>"
+	template:"<h1>hello world</h1>";
 });
 
-
 var router = new VueRouter();
-
-
 router.map({
 	'/':{
 		component: postList
 	}
 });
-
 
 router.start(App, '#app');
 
